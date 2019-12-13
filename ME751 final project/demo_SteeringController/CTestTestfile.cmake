936db24c62ab557bbfdf68bf176134d99e8e8882
GongdaChen <58753764+GongdaChen@users.noreply.github.com>
2019-12-13 00:02:06 -0600

    Create CTestTestfile.cmake

---
+++ ME751 final project/demo_SteeringController/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrator/Documents/chrono/chrono-develop/src/demos/vehicle/demo_SteeringController
+# Build directory: C:/Users/Administrator/Documents/chrono/chrono-build/src/demos/vehicle/demo_SteeringController
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
